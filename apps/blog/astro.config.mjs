ab0fefe47f33fed125109bdb8415a9647353687e
Grady Kuhnline <github@heygrady.net>
2022-08-31 03:59:14 +0000

    fix: preload fonts and enable prefetch for links

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,23 +1,25 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://heygrady.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), prefetch()],
   markdown: {
     shikiConfig: {
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'solarized-light',
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: ['astro', 'js', 'jsx', 'ts', 'tsx', 'sh'],
-      wrap: true,
-    },
+      wrap: true
+    }
   },
   vite: {
     build: {
-      assetsInlineLimit: 1024,
-    },
-  },
-})
+      assetsInlineLimit: 1024
+    }
+  }
+});
