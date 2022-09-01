daf85fcf937c4e1fe52f2bc6bbfa32882a03dfee
Grady Kuhnline <github@heygrady.net>
2022-09-01 07:11:25 +0000

    chore: enable create-post command

---
+++ heygrady.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // which package contains our app
+  packageName: '@heygrady/blog-app',
+
+  // where do we store posts (relative to app package root)
+  postsDir: 'src/pages/posts',
+}
