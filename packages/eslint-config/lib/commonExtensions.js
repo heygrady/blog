5a9d2211409b75f405655155e85f05dffdb42c44
Grady Kuhnline <github@heygrady.net>
2022-09-01 01:27:42 +0000

    feat(eslint-config): separate astro into another package

--- packages/eslint-config/lib/commonExtensions.js
+++ packages/eslint-config/lib/commonExtensions.js
@@ -1,6 +1,6 @@
 const nodeExtensions = ['.cjs', '.mjs', '.js', 'jsx']
 const typescriptExtensions = ['.cts', '.mts', '.ts', '.tsx']
-const allExtensions = [...nodeExtensions, typescriptExtensions]
+const allExtensions = [...nodeExtensions, ...typescriptExtensions]
 
 module.exports = {
   allExtensions,
