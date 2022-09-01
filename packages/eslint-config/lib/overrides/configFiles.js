9ca6bccedc594103df3ffe13fa6dd58765983df7
Grady Kuhnline <github@heygrady.net>
2022-09-01 03:16:09 +0000

    fix(eslint-config): debug common rules

--- packages/eslint-config/lib/overrides/configFiles.js
+++ packages/eslint-config/lib/overrides/configFiles.js
@@ -1,10 +1,13 @@
 // common config overrides for all configs
 module.exports = [
   {
-    files: ['*.config.{js,mjs}', '*rc.{js,mjs}'],
+    files: ['*.config.{js,cjs,mjs}', '*rc.{js,cjs,mjs}'],
     rules: {
       'import/no-default-export': 'off',
       'import/no-anonymous-default-export': 'off',
+      'import/no-unresolved': 'warn',
+      'n/no-unpublished-import': 'off',
+      'n/no-unpublished-require': 'off',
     },
   },
 ]
--- packages/eslint-config/lib/rules/common.js
+++ packages/eslint-config/lib/rules/common.js
@@ -1,4 +1,6 @@
 // common rules for all configs
 module.exports = {
   'no-unused-vars': 'warn',
+  'n/no-unpublished-import': 'off',
+  'n/no-unpublished-require': 'off',
 }
