63e9a5da093e6837476fdac632cfe6f1538357c2
Grady Kuhnline <github@heygrady.net>
2022-09-01 03:16:09 +0000

    fix(eslint-config): debug common rules

--- packages/eslint-config/lib/overrides/typescript.js
+++ packages/eslint-config/lib/overrides/typescript.js
@@ -27,6 +27,7 @@ module.exports = [
         },
       },
     },
+    parser: '@typescript-eslint/parser',
     parserOptions: {
       project: `${root}/tsconfig.json`,
       tsconfigRootDir: root,
@@ -36,6 +37,51 @@ module.exports = [
       'import/extensions': 'off',
       // https://github.com/weiran-zsd/eslint-plugin-node/issues/47
       'n/no-missing-import': 'off',
+      'n/no-unsupported-features/es-syntax': [
+        'error',
+        { ignores: ['modules'] },
+      ],
+    },
+  },
+  // Parse js files within a Typescript src folder
+  {
+    files: ['src/**/*.{js,jsx,cjs,mjs}'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: `${root}/tsconfig.json`,
+      tsconfigRootDir: root,
+    },
+    settings: {
+      'import/extensions': allExtensions,
+      'import/parsers': {
+        '@typescript-eslint/parser': typescriptExtensions,
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          extensions: typescriptExtensions,
+          project: `${root}/tsconfig.json`,
+        },
+        node: {
+          extensions: allExtensions,
+        },
+      },
+    },
+    rules: {
+      'import/extensions': 'off',
+      // https://github.com/weiran-zsd/eslint-plugin-node/issues/47
+      'n/no-missing-import': 'off',
+      'n/no-unsupported-features/es-syntax': [
+        'error',
+        { ignores: ['modules'] },
+      ],
+    },
+  },
+  // Fixes collision with the markdown settings when matching for `src` above.
+  {
+    files: ['**/*.md/**'],
+    parserOptions: {
+      project: null,
     },
   },
 ]
--- packages/eslint-config/lib/rules/node.js
+++ packages/eslint-config/lib/rules/node.js
@@ -1,8 +1,6 @@
 // common node rules for all configs
 module.exports = {
   'n/no-process-exit': 'warn',
-  'n/no-unpublished-require': [
-    'error',
-    { allowModules: ['@rushstack/eslint-patch'] },
-  ],
+  'n/no-unpublished-import': 'off',
+  'n/no-unpublished-require': 'off',
 }
