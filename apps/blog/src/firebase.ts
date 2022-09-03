5cd2c0ce3dcbae818a0f6e8b37a5f4b0750e3a0e
Grady Kuhnline <github@heygrady.net>
2022-09-03 01:41:11 +0000

    feat(blog-app): init firebase app

---
+++ apps/blog/src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, FirebaseApp } from 'firebase/app'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAiP6-WAL42i_FwSLYFsedAPPPCI_j-ctY',
+  authDomain: 'blog-7e7a6.firebaseapp.com',
+  databaseURL: 'https://blog-7e7a6.firebaseio.com',
+  projectId: 'blog-7e7a6',
+  storageBucket: 'blog-7e7a6.appspot.com',
+  messagingSenderId: '180239026597',
+  appId: '1:180239026597:web:48ff1e056501980338c04c',
+}
+
+// Initialize Firebase
+export const init = (): FirebaseApp => {
+  const app = initializeApp(firebaseConfig)
+  return app
+}
