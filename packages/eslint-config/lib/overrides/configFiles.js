b1ae28b7ef6f8df3259313e3655093013a10c1a2
Grady Kuhnline <github@heygrady.net>
2023-07-27 19:56:12 -0700

    feat: fix astra config

--- packages/eslint-config/lib/overrides/configFiles.js
+++ packages/eslint-config/lib/overrides/configFiles.js
@@ -1,8 +1,11 @@
+const { parserServicesRules } = require('../parserServicesRules.js')
+
 // common config overrides for all configs
 module.exports = [
   {
     files: ['*.config.{js,cjs,mjs}', '*rc.{js,cjs,mjs}'],
     rules: {
+      ...parserServicesRules,
       'import/no-default-export': 'off',
       'import/no-anonymous-default-export': 'off',
       'import/no-unresolved': 'warn',
