e4adf5ad07eb66eb7ca82c26338da082ecf4ed34
Grady Kuhnline <github@heygrady.net>
2023-07-28 19:36:14 -0700

    feat: support vitest, typescript improvements

--- packages/eslint-config/lib/overrides/configFiles.js
+++ packages/eslint-config/lib/overrides/configFiles.js
@@ -1,11 +1,11 @@
-const { parserServicesRules } = require('../parserServicesRules.js')
-
 // common config overrides for all configs
 module.exports = [
   {
     files: ['*.config.{js,cjs,mjs,ts,cts,mts}', '*rc.{js,cjs,mjs,ts,cts,mts}'],
+    parserOptions: {
+      project: null,
+    },
     rules: {
-      ...parserServicesRules,
       'import/no-default-export': 'off',
       'import/no-anonymous-default-export': 'off',
       'import/no-unresolved': 'warn',
