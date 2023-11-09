c83977f9af89e953f3a7bca5710cd75dd9c9921f
Grady Kuhnline <github@heygrady.net>
2023-11-09 13:46:25 -0800

    feat: disable prettier in astro files

--- packages/eslint-config-astro/.prettierrc.js
+++ packages/eslint-config-astro/.prettierrc.js
@@ -1,13 +1,4 @@
 module.exports = {
   ...require('prettier-config-standard'),
-  plugins: [require.resolve('prettier-plugin-astro')],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro',
-      },
-    },
-  ],
   trailingComma: 'es5',
 }
