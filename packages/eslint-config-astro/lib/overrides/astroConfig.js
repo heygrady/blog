eaaaa0a89a25f0d80936a99087d875ffa2c4126d
Grady Kuhnline <github@heygrady.net>
2025-11-30 11:23:31 -0800

    chore(eslint-config): remove eslint-config-astro package
    The Astro configuration has been merged into the main
    @heygrady/eslint-config package as the astro.js export.
    This package is no longer needed.

--- packages/eslint-config-astro/lib/overrides/astroConfig.js
+++
@@ -1,9 +0,0 @@
-// common config overrides for all configs
-module.exports = [
-  {
-    files: ['astro.config.{js,cjs,mjs}'],
-    rules: {
-      'import/no-unresolved': ['error', { ignore: ['^@astrojs\\/'] }],
-    },
-  },
-]
--- packages/eslint-config-astro/lib/rules/prettier.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'prettier/prettier': [
-    'error',
-    require('../../.prettierrc.js'),
-    {
-      usePrettierrc: false,
-    },
-  ],
-}
